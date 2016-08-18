a120403214e43154225c3ae1de9139e21041d39e
FeiXiang Yan <currygiafan@gmail.com>
2016-08-19 00:16:44 +0800

    update

---
+++ src/leetcode/HouseRobberIII.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import leetcode.struct.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by gaifan on 2016/8/18.
+ */
+public class HouseRobberIII {
+    public int rob(TreeNode root) {
+        return rob(root, new HashMap<TreeNode, Integer>());
+    }
+
+    public int rob(TreeNode root, Map<TreeNode, Integer> map) {
+        if (map.containsKey(root))
+            return map.get(root);
+
+        if (root == null)
+            return 0;
+
+        int max = root.val;
+        if (root.left != null)
+            max += (rob(root.left.left) + rob(root.left.right));
+        if (root.right != null)
+            max += (rob(root.right.left) + rob(root.right.right));
+
+        int left = rob(root.left);
+        int right = rob(root.right);
+        max = Math.max(max, left + right);
+        map.put(root, max);
+        return max;
+
+
+    }
+
+    public static void main(String[] args) {
+        TreeNode tn = new TreeNode("3,2,3,#,3,#,1");
+        System.out.println(new HouseRobberIII().rob(tn));
+        TreeNode tn1 = new TreeNode("3,4,5,1,3,#,1");
+        System.out.println(new HouseRobberIII().rob(tn1));
+    }
+
+}
